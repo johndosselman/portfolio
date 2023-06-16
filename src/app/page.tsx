80ecf45f565e233fd63c27e3b4a3018b849413d2
John Dosselman <johndosselman@gmail.com>
2023-06-15 20:49:34 -0500

    remove priority from profilePic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -349,7 +349,6 @@ export default function Home() {
                   e.preventDefault();
                 }}
                 draggable={false}
-                priority
               />
             </div>
             <div>
